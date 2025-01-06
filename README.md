a5d54da47f44dc54266254700ce407c15676e6fc
Jennifer Cheung <cheung.jenn@northeastern.edu>
2025-01-06 17:55:23 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# jeniichee.github.io
+# Personal Website [In Progress] 
+This is my personal website created entirely with HTML, CSS, and a little JavaScript! Currently in development, feel free to visit at jeniichee.github.io :) 
---
+++ script.js
@@ -0,0 +1,95 @@
+// Typewriter created by Tameem Safi.
+// source: https://github.com/tameemsafi/typewriterjs
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('hello!')
+    .pauseFor(1000)
+    .deleteAll()
+
+    .typeString('nice to meet you!')
+    .pauseFor(1500)
+    .deleteAll()
+
+    .typeString('my name is jen.')
+    .pauseFor(1500)
+    .deleteAll()
+
+    .typeString('i am studying cs and neuro.')
+    .pauseFor(1500)
+    .deleteAll() 
+
+    .typeString('i am also interested in design and product development.')
+    .pauseFor(1500)
+    .deleteAll() 
+
+    .typeString('my work serves to empower others')
+    .pauseFor(1500)
+    .deleteAll()
+
+    .typeString('and enhance lives.')
+    .pauseFor(1500)
+    .deleteAll()
+
+    .typeString('in my free time, i like to watch k-dramas.')
+    .pauseFor(1500)
+    .deleteAll()
+
+    .typeString('i am also a coffee and tea enthusiast.')
+    .pauseFor(1500)
+    .deleteAll()
+
+    .typeString('thank you for visiting!')
+    .pauseFor(1500)
+    .deleteAll()
+
+    .typeString('enjoy the rest of your day :)')
+    .pauseFor(1500)
+    .deleteAll()
+
+    .start();
+
+// dark mode
+
+// function dark() {
+//     var element = document.body;
+//     element.classList.toggle("dark-mode");
+//   }
+
+// open accordian 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+// navbar scroll 
+window.addEventListener("scroll", scroll);
+
+function scroll() {
+  const header = document.querySelector("#header").offsetHeight;
+  const navbar = document.querySelector(".nav")
+  
+  if (window.pageYOffset >= header) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+  
